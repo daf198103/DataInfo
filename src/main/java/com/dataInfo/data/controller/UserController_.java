f4352168839dce5e026fa57beff641d612a24ba0
daf198103 <david.franca01@fatec.sp.gov.br>
2019-10-06 02:00:37 -0300

    revisao

--- src/main/java/com/dataInfo/data/controller/UserController_.java
+++ src/main/java/com/dataInfo/data/controller/UserController_.java
@@ -6,16 +6,21 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
 import com.dataInfo.data.model.User_;
 import com.dataInfo.data.service.UserService_;
 
+
 @RestController 
 @RequestMapping(value = "/api")
+@CrossOrigin(origins = "http://localhost:4200", allowedHeaders = "*")
 public class UserController_ 
 {
 	
@@ -31,12 +36,14 @@ public class UserController_
 	}
 
 	
+	
 	@RequestMapping(value = "/getAll", method = RequestMethod.GET)
     public ResponseEntity<List<User_>> searchAll() {
         List<User_> users = userService.findAll();
         return new ResponseEntity<List<User_>>(users, HttpStatus.OK);
     }	
 
+	
 	@RequestMapping(value = "/byname/{name}", method = RequestMethod.GET)
     public ResponseEntity<User_> searchByName(@PathVariable("name") String name)
     {
@@ -49,7 +56,7 @@ public class UserController_
         return new ResponseEntity<User_>(user, new HttpHeaders(), HttpStatus.OK);
     }
 	
-	
+    
     @RequestMapping(value = "/bycpf/{cpf}", method = RequestMethod.GET)
 	public ResponseEntity<User_> searchByCpf(@PathVariable("cpf") String cpf)
 	{
@@ -61,24 +68,25 @@ public class UserController_
 		return new ResponseEntity<User_>(user, new HttpHeaders(),HttpStatus.OK);
 	}
 	
-	
-	@RequestMapping(value = "/save", method = RequestMethod.POST)
+	@PostMapping(value = "/create/save")//, method = RequestMethod.POST
     public ResponseEntity<User_> save(@RequestBody User_ user) 
     {
-	  userService.save(user); 
+    	userService.save(user);  
 	  return new ResponseEntity<User_>(user, new  HttpHeaders(),HttpStatus.OK); 
     }
 	
-	
-    @RequestMapping(value = "/save", method = RequestMethod.PUT)
+    
+    @RequestMapping(value = "/update/save")//, method = RequestMethod.POST
 	public ResponseEntity<User_> update(@RequestBody User_ user)
 	{
+		
 		User_ usu = userService.findByCpf(user.getCpf());
 		usu.setCpf(user.getCpf());usu.setEmail(user.getEmail());usu.setAccessProfile(user.getAccessProfile());usu.setFunction(user.getFunction());
 		usu.setPhone(user.getPhone());usu.setSituation(user.getSituation());usu.setUserName(user.getUserName());
 		userService.save(usu);
 		return new ResponseEntity<User_>(usu, new HttpHeaders(),HttpStatus.OK);
 	}
+	
 	
 	 @RequestMapping(value = "/delete/{cpf}", method = RequestMethod.DELETE)
 	 public ResponseEntity<User_> deleteByCpf(@PathVariable("cpf") String cpf) {
